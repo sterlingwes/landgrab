2f854ea462523eb286a95dc69afa3c17f70f2ca5
Wes Johnson <swj@wesquire.ca>
2020-06-26 06:57:32 -0400

    add catch for misused refilter

--- src/refilter.js
+++ src/refilter.js
@@ -3,4 +3,7 @@ const { formatResults } = require("./serialize");
 const filename = process.argv.slice(0).pop();
 const file = require(filename);
 
+if (!filename || !file)
+  throw new Error("Please specify a JSON file to apply the filter on");
+
 formatResults("test-reformat.json", file);
