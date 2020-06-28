2c99edaf1f1ea06fe57c85d820de5536ac273c59
Wes Johnson <swj@wesquire.ca>
2020-06-28 12:05:18 -0400

    add support for JSX and update listing.js to use

--- src/site/listings.js
+++ src/site/listings.js
@@ -1,8 +1,9 @@
+const { createElement } = require("../../lib/jsx-without-react");
 const listings = require("./augmented-listings.json");
 
-const addItem = (listingDiv) => {
-  const container = document.getElementById("listings");
-  container.appendChild(listingDiv);
+const mount = (rootElement) => {
+  const container = document.body;
+  container.appendChild(rootElement);
 };
 
 const el = (elementName) => document.createElement(elementName);
@@ -13,74 +14,142 @@ const currencyFormatter = Intl.NumberFormat("en-CA", {
 });
 const formatCurrency = (value) => currencyFormatter.format(value);
 
-const createListingDiv = (listing) => {
-  const div = el("div");
-  div.className = "listing";
-  const imageContainer = el("div");
-  imageContainer.className = "listing-images";
-  const header = el("div");
-  header.className = "listing-header";
+const Header = ({ children }) => (
+  <h2 style={{ margin: 0, marginBottom: 5, padding: 0 }}>{children}</h2>
+);
 
-  if (listing.Property.Photo) {
-    listing.Property.Photo.forEach((photo) => {
-      const img = el("img");
-      img.src = photo.MedResPath || photo.LowResPath || photo.HighResPath;
-      imageContainer.appendChild(img);
-    });
-  }
+const SubHeader = ({ children }) => (
+  <h3 style={{ margin: 0, marginBottom: 10, padding: 0 }}>{children}</h3>
+);
 
-  const h2 = el("h2");
-  h2.innerText = listing.Property.Address.AddressText;
-
-  const price = el("span");
-  price.className = "listing-price";
-  price.innerText = listing.Property.Price;
-
-  const distances = el("ul");
-  const closeCities = Object.entries(listing.Distances);
-  if (closeCities.length) {
-    const cities = closeCities.sort((a, b) => a[1] - b[1]).slice(0, 3);
-    cities.forEach((city) => {
-      const item = el("li");
-      const cityDistance = Math.round(city[1] * 100) / 100;
-      item.innerText = `${city[0]} (${cityDistance}kms)`;
-      distances.appendChild(item);
-    });
-  }
-
-  const ppa = listing["Price per acre"];
-  if (ppa) {
-    const ppaRound = formatCurrency(Math.round(ppa * 100) / 100);
-    const acres = Math.round(listing.Acreage * 10) / 10;
-    price.innerText += ` (${acres}ac, ${ppaRound} per acre)`;
-  }
-
-  header.appendChild(h2);
-  header.appendChild(price);
-  header.appendChild(distances);
-
-  const description = el("p");
-  description.className = "listing-description";
-  description.innerText = listing.Description;
-
-  const detailLink = el("a");
-  detailLink.href = listing["Detail URL"];
-  detailLink.innerText = "View Listing";
-  detailLink.className = "listing-detaillink";
-
-  const mapsLink = el("a");
-  mapsLink.className = "listing-maplink";
-  const lat = listing.Property.Address.Latitude;
-  const lon = listing.Property.Address.Longitude;
-  mapsLink.href = `https://www.google.ca/maps/@${lat},${lon},10z`;
-
-  div.appendChild(imageContainer);
-  div.appendChild(header);
-  div.appendChild(description);
-  div.appendChild(detailLink);
-  div.appendChild(mapsLink);
-
-  return div;
+const layoutStyle = {
+  display: "flex",
+  flexDirection: "row",
 };
 
-listings.forEach((listing) => addItem(createListingDiv(listing)));
+const SplitLayout = ({ children }) => (
+  <div className="split-layout" style={layoutStyle}>
+    {children}
+  </div>
+);
+
+const Price = ({ price, ppa, acreage }) => {
+  let acreageStats;
+  if (ppa) {
+    const ppaRound = formatCurrency(Math.round(ppa * 100) / 100);
+    const acres = Math.round(acreage * 10) / 10;
+    acreageStats = `(${acres}ac, ${ppaRound} per acre)`;
+  }
+
+  return (
+    <div>
+      <span>{price}</span>
+      {!!acreageStats && <span>{acreageStats}</span>}
+    </div>
+  );
+};
+
+const CloseCities = ({ distances }) => {
+  const closeCities = Object.entries(distances);
+  const cities = closeCities.sort((a, b) => a[1] - b[1]).slice(0, 3);
+
+  return (
+    <ul>
+      {cities.map((city) => {
+        const cityDistance = Math.round(city[1] * 100) / 100;
+        return <li>{`${city[0]} (${cityDistance}kms)`}</li>;
+      })}
+    </ul>
+  );
+};
+
+const Description = ({ children }) => (
+  <div>
+    {typeof children === "string" ? children.replace("***", "") : children}
+  </div>
+);
+
+const Images = ({ images }) => (
+  <div>
+    {images.map((img) => (
+      <img style="width: 256px; height: 200px;" />
+    ))}
+  </div>
+);
+
+const Card = ({ children }) => (
+  <div
+    style={{
+      position: "relative",
+      margin: 20,
+      padding: 20,
+      borderRadius: 5,
+      backgroundColor: "#fff",
+      boxShadow: "2px 2px 2px #eee",
+    }}
+  >
+    {children}
+  </div>
+);
+
+const ListingId = ({ children }) => (
+  <div
+    style={{
+      position: "absolute",
+      top: 10,
+      right: 10,
+      fontSize: 12,
+      color: "#ccc",
+    }}
+  >
+    {children}
+  </div>
+);
+
+const Listing = ({
+  Property: {
+    Address: { AddressText, Latitude: lat, Longitude: lon },
+    Photo,
+    Price: price,
+  },
+  Description: description,
+  Distances: distances,
+  Acreage: acreage,
+  id,
+  ...rest
+}) => {
+  const ppa = rest["Price per acre"];
+  const detailUrl = rest["Detail URL"];
+  const [address, location] = AddressText.split("|");
+  return (
+    <Card>
+      <Header>{address}</Header>
+      {!!location && <SubHeader>{location}</SubHeader>}
+      <SplitLayout>
+        {!!Photo && <Images images={Photo} />}
+        <div style={{ marginLeft: 20 }}>
+          <Price price={price} ppa={ppa} acreage={acreage} />
+          <CloseCities distances={distances} />
+          <Description>{description}</Description>
+          <div>
+            <a href={detailUrl}>Listing Detail</a>
+            <a href={`https://www.google.ca/maps/@${lat},${lon},10z`}>
+              Google Maps
+            </a>
+          </div>
+        </div>
+      </SplitLayout>
+      <ListingId>{id}</ListingId>
+    </Card>
+  );
+};
+
+const container = (
+  <div>
+    {listings.map((listing) => (
+      <Listing {...listing} />
+    ))}
+  </div>
+);
+
+mount(container);
