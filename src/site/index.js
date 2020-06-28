44c50e4da9183a5c4a1a27df07ea9ec01b53f207
Wes Johnson <swj@wesquire.ca>
2020-06-28 15:50:50 -0400

    index site doesn't need html ext

--- src/site/index.js
+++ src/site/index.js
@@ -5,7 +5,7 @@ const dayList = document.getElementById("days");
 const addItem = (date) => {
   const li = document.createElement("li");
   const link = document.createElement("a");
-  link.href = `${date}.html`;
+  link.href = date;
   link.innerText = date;
   li.appendChild(link);
   dayList.appendChild(li);
