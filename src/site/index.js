f16b1b5ccf93341d0253c5347cfe00cc567fce51
Wes Johnson <swj@wesquire.ca>
2020-07-02 08:13:36 -0400

    fix index script

--- src/site/index.js
+++ src/site/index.js
@@ -5,14 +5,15 @@ fetches.reverse();
 const dayList = document.getElementById("days");
 
 const summarizeChanges = (changeStats) => {
-  return `+${changeStats.added.length}, -${changeStats.removed.length}, Δ${changeStats.changed.length}`;
+  if (!changeStats) return;
+  return `(+${changeStats.added.length}, -${changeStats.removed.length}, Δ${changeStats.changed.length})`;
 };
 
 const addItem = ({ date, changeStats }) => {
   const li = document.createElement("li");
   const link = document.createElement("a");
   link.href = date;
-  link.innerText = `${date} (${summarizeChanges(changeStats)}`;
+  link.innerText = `${date} ${summarizeChanges(changeStats)}`;
   li.appendChild(link);
   dayList.appendChild(li);
 };
