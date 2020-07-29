d5e6351171a6bd432f0ede9687b346e2fc4ffa82
Wes Johnson <swj@wesquire.ca>
2020-07-29 07:37:21 -0400

    include DSL internet

--- src/check-internet.js
+++ src/check-internet.js
@@ -14,7 +14,6 @@ const detailPagePath = (result) => {
 
 const excludableInternetTypes = [
   "Fixed Wireless",
-  "DSL",
   "Mobile Wireless",
   "Satellite",
   "High Capacity Transport Services",
